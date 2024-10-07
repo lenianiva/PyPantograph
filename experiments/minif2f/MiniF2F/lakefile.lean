034fd458e31fa175e7a7e2881be40a836634548b
Leni Aniva <v@leni.sh>
2024-10-06 22:04:10 -0700

    chore: Update Pantograph and Lean version to 4.12

--- experiments/minif2f/MiniF2F/lakefile.lean
+++ experiments/minif2f/MiniF2F/lakefile.lean
@@ -2,9 +2,9 @@ import Lake
 open Lake DSL
 
 require aesop from git
-  "https://github.com/leanprover-community/aesop.git" @ "v4.10.0-rc1"
+  "https://github.com/leanprover-community/aesop.git" @ "v4.12.0"
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.10.0-rc1"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.12.0"
 
 package MiniF2F
 
