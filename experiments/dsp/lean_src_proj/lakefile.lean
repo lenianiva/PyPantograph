a896e65f693a008c6d8721db83c5fb16b0457e84
Leni Aniva <v@leni.sh>
2024-10-06 22:04:10 -0700

    chore: Update Pantograph and Lean version to 4.12

--- experiments/dsp/lean_src_proj/lakefile.lean
+++ experiments/dsp/lean_src_proj/lakefile.lean
@@ -3,9 +3,9 @@ open Lake DSL
 
 
 require aesop from git
-  "https://github.com/leanprover-community/aesop.git" @ "v4.10.0-rc1"
+  "https://github.com/leanprover-community/aesop.git" @ "v4.12.0"
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.10.0-rc1"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.12.0"
 
 package LeanSrcProj
 @[default_target]
