bc5a361a08a8eb270910819fae663fb06b085dc6
Leni Aniva <v@leni.sh>
2024-10-21 21:56:45 -0700

    doc: Artefact packing instructions

--- experiments/dsp/pack-artefact.sh
+++ experiments/dsp/pack-artefact.sh
@@ -5,7 +5,7 @@
 
 echo "Packing Git archive"
 git archive --format zip --output artefact.zip HEAD
-#echo "Packing docker image"
-#docker image save pantograph > pantograph.tar
+echo "Packing docker image"
+docker image save chrysoberyl/pantograph > pantograph.tar
 echo "Adding experimental results"
-zip -ur artefact.zip result/ref-4o/{valid1,valid3,test1,test3} result/ref-o1/{valid1,valid3} #pantograph.tar
+zip -ur artefact.zip result/ref-4o/{valid1,valid3,test1,test3} result/ref-o1/{valid1,valid3} pantograph.tar
