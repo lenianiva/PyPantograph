9eed374542de1f69f29973c107c0693014291b23
Attila Jamilov <32287233+attilajam@users.noreply.github.com>
2025-01-27 21:35:57 -0800

    Make the code more concise

--- build.py
+++ build.py
@@ -11,11 +11,8 @@ with subprocess.Popen(["lake", "build", "repl"], cwd=PATH_PANTOGRAPH) as p:
     p.wait()
 
 path_executable = PATH_PY / "pantograph-repl"
-if os.name == 'nt': # check that the OS is Windows or not
-    shutil.copyfile(PATH_PANTOGRAPH / ".lake/build/bin/repl.exe", path_executable) 
-else:
-    shutil.copyfile(PATH_PANTOGRAPH / ".lake/build/bin/repl", path_executable)
+repl_src = "repl.exe" if os.name == "nt" else "repl"
+shutil.copyfile(PATH_PANTOGRAPH / f".lake/build/bin/{repl_src}", path_executable)
 os.chmod(path_executable, os.stat(path_executable).st_mode | stat.S_IEXEC)
-
 # -- Copy the Lean toolchain file to the specified path
 shutil.copyfile(PATH_PANTOGRAPH / "lean-toolchain", PATH_PY / "lean-toolchain")
